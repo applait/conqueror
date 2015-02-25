0ae479e82d05fc6942ce1824e308758d9e60ca3e
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-02-25 10:59:21 +0530

    Remove cookie-parser and express-session till we need them
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var express = require("express"),
     path = require("path"),
     fs = require("fs"),
     bodyParser = require("body-parser"),
-    session = require("express-session"),
-    cookieParser = require("cookie-parser"),
     config = require("./config");
 
 var app = express();
@@ -13,10 +11,8 @@ var app = express();
 // Configure application
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser("banana"));
 app.use("/assets", express.static(path.join(__dirname, 'assets')));
 app.use("/", express.static(path.join(__dirname, 'static')));
-app.use(session({ secret: "potato" }));
 
 // Start the server
 var server = app.listen(config.APP_PORT, config.APP_IP, function () {
