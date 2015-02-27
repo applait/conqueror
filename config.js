976db7271ec1691046d84e64cc2f968b82a6460c
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-02-27 11:59:49 +0530

    Hardcode kurento server address in config
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 var config = {
     APP_IP : "0.0.0.0",
-    APP_PORT: "8080"
+    APP_PORT: "8080",
+    KURENTO_HOST: "ws://conq-kurento.kaustavdm.in",
+    KURENTO_PORT: "8888"
 };
+
+config.KURENTO_URL = config.KURENTO_HOST + ":" + config.KURENTO_PORT + "/kurento";
 
 module.exports = config;
