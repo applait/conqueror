a836509520b9bc771815fcaa2d002d6d1658da45
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-03-15 00:06:59 +0530

    First attempt at creating hub-based WebRTC-endpoint session
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- api/create.js
+++ api/create.js
@@ -20,22 +20,72 @@ router.post("/", function (req, res) {
         return res.status(406).json({ "message": "Need `name` as a query parameter." });
     }
 
-    // Prepare data object
-    var data = {
-        members: [{ ip: req.ip, name: name, joined: Date.now(), quit: null }],
-        meta: {
-            created: Date.now(),
-            creator: name
-        }
-    };
-
-    // Put id in session db
-    cq.db.sessions.put(id, data, function (err) {
-        if (err) {
-            console.log("[ERR] Creating session", id, req.ip);
+    // Auth passed. Create MediaPipeline
+    cq.kurento.create("MediaPipeline", function (error, pipeline) {
+        if (error) {
+            console.log("MediaPipeline error", error);
             return res.status(500).json({ "message": "Oops! Error! API has gone nuts." });
         }
-        res.status(200).json({ "message": "Session created", session: { id: id, data: data }});
+
+        console.log("MediaPipeline created", pipeline.id);
+
+        // Create webrtc endpoint at this point?
+
+        pipeline.create("Mixer", function (error, mixer) {
+            if (error) {
+                console.log("Mixer error", error);
+                return res.status(500).json({ "message": "Oops! Error! API has gone nuts." });
+            }
+            console.log("Mixer created", mixer.id);
+
+            cq.kurento.create("HubPort", { "hub": mixer }, function (error, hubport) {
+                if (error) {
+                    console.log("HubPort error", error);
+                    return res.status(500).json({ "message": "Oops! Error! API has gone nuts." });
+                }
+                console.log("HubPort created", hubport.id);
+
+                pipeline.create("WebRtcEndpoint", function (error, webrtc) {
+                    if (error) {
+                        console.log("WebRtcEndpoint error", error);
+                        return res.status(500).json({ "message": "Oops! Error! API has gone nuts." });
+                    }
+                    console.log("WebRtcEndpoint created", webrtc.id);
+
+                    hubport.connect(webrtc, function (error) {
+                        if (error) {
+                            console.log("HubPort connection error", error);
+                            return res.status(500).json({ "message": "Oops! Error! API has gone nuts." });
+                        }
+
+                        console.log("Hubport %s connected to WebRtcEndpoint %s", hubport.id, webrtc.id);
+
+                        var datetime = new Date();
+                        // Prepare data object
+                        var data = {
+                            members: [{ ip: req.ip, name: name, joined: datetime, quit: null,
+                                        hubport: hubport, webrtc: webrtc }],
+                            meta: {
+                                created: datetime,
+                                creator: name
+                            },
+                            pipeline: pipeline,
+                            mixer: mixer
+                        };
+
+                        // Put id in session db
+                        cq.db.sessions.put(id, data, function (err) {
+                            if (err) {
+                                console.log("[ERR] Creating session", id, req.ip);
+                                return res.status(500).json({ "message": "Oops! Error! API has gone nuts." });
+                            }
+                            console.log("Session created", id, data.meta.created);
+                            res.status(200).json({ "message": "Session created", session: { id: id, data: data }});
+                        });
+                    });
+                });
+            });
+        });
     });
 });
 
