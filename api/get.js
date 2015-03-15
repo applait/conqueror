1078a69d4746178f7e7ba748e666c20cb228f034
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-03-15 21:03:46 +0530

    Basic implemention of getting a call session details
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- api/get.js
+++ api/get.js
@@ -4,28 +4,20 @@
  * Get session info
  */
 
-var router = require("express").Router();
-
-router.get("/:id", function (req, res) {
-    var id = req.params && req.params.id && req.params.id.trim();
+module.exports = function (data, callback, socket) {
+    var id = data && data.sessionid && data.sessionid.trim();
 
     if (!id) {
-        return res.status(406).json({ "message": "Need a session id" });
+        return callback({ "message": "Need a session id" });
     }
 
     // Fetch session
-    cq.db.sessions.get(id, function (err, data) {
+    cq.db.sessions.get(id, function (err, session) {
         if (err) {
             console.log("[ERR] Fetching session", id, err);
-            return res.status(406).json({ "message": "Not right." });
+            return callback({ "message": "Not right." });
         }
 
-        res.status(200).json({ "message": "Session info", "session": { "id": id, "data": data }});
+        return callback(null, { "message": "Session info", "session": { "id": id, "data": data }});
     });
-});
-
-router.post("/", function (req, res) {
-    res.status(406).json({ "message": "Need a session id." });
-});
-
-module.exports = router;
+};
