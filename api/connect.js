df85b011f14c1e0c4557e9d5cb7dca0f1e50f08a
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-03-15 21:04:03 +0530

    Sample of implementing sockets and retrieving media objects
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- api/connect.js
+++ api/connect.js
@@ -4,53 +4,60 @@
  * Connect to existing connections
  */
 
-var router = require("express").Router();
+var util = require("util");
 
-router.post("/:id", function (req, res) {
-    var id = req.params && req.params.id && req.params.id.trim(),
-        name = req.body && req.body.name && req.body.name.trim();
+var q = function (c) {
+    return util.inspect(c, {showHidden: true, depth: null });
+};
+
+module.exports = function (data, callback, socket) {
+    var id = data && data.sessionid && data.sessionid.trim(),
+        name = data && data.name && data.name.trim();
 
     if (!id) {
-        return res.status(406).json({ "message": "Need a connection id." });
+        return callback({ "message": "Need a connection id." });
     }
 
     if (!name) {
-        return res.status(406).json({ "message": "Need name as a query parameter." });
+        return callback({ "message": "Need name as a query parameter." });
     }
 
     // Fetch session
-    cq.db.sessions.get(id, function (err, data) {
+    cq.db.sessions.get(id, function (err, session) {
         if (err) {
             console.log("[ERR] Fetching session", id, err);
-            return res.status(406).json({ "message": "Not right." });
+            return callback({ "message": "Not right." });
         }
 
         // Match name
-        var namematch = data.members.filter(function (el) {
+        var namematch = session.members.filter(function (el) {
             return el.name === name;
         });
 
         if (namematch.length) {
-            return res.status(406).json({ "message": "Name already in use. Could not join session." });
+            return callback({ "message": "Name already in use. Could not join session." });
         }
 
-        // Create new user info
-        var currentuser = { ip: req.ip, name: name, joined: Date.now(), quit: null };
-
-        // Add to current session info
-        cq.db.sessions.put(id, data.members.push(currentuser), function (err) {
-            if (err) {
+        cq.kurento.getMediaobjectById(session.pipeline.id, function (error, pipeline) {
+            if (error) {
                 console.log("[ERR] Updating session", id, err);
-                return res.status(500).json({ "message": "Can't join session.'" });
+                return callback({ "message": "Can't join session." });
             }
 
-            res.status(200).json({ "message": "Session joined", "session": { "id": id, "data": data }});
+            // Create new user info
+            var currentuser = { ip: null, name: name, joined: Date.now(), quit: null };
+
+            session.members.push(currentuser);
+
+            // Add to current session info
+            cq.db.sessions.put(id, session, function (err) {
+                if (err) {
+                    console.log("[ERR] Updating session", id, err);
+                    return callback({ "message": "Can't join session." });
+                }
+
+                return callback(null, { "message": "Session joined", "session": { "id": id, "data": data }});
+            });
         });
     });
-});
-
-router.post("/", function (req, res) {
-    res.status(406).json({ "message": "Need a connection id." });
-});
-
-module.exports = router;
+};
