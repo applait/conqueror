35e30ff23c4a2721e12b521ee4ebc7e40836bce5
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-03-22 16:18:14 +0530

    Implement api/connect using promises
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- api/connect.js
+++ api/connect.js
@@ -7,8 +7,16 @@
 module.exports = function (data, callback, socket) {
     var id = data && data.sessionid && data.sessionid.trim(),
         name = data && data.name && data.name.trim(),
-        sdpoffer = data && data.sdpOffer && data.sdpOffer.trim();
+        sdpoffer = data && data.sdpOffer && data.sdpOffer.trim(),
+        pipeline = null,
+        hub = null,
+        hubport = null,
+        session = null;
 
+    var onerror = function (error) {
+        console.log("ERROR", error);
+        return callback({ "message": "Oops! Error! API has gone nuts." });
+    };
 
     if (!id) {
         return callback({ "message": "Need `sessionid` to be passed in the data." });
@@ -22,12 +30,70 @@ module.exports = function (data, callback, socket) {
         return callback({ "message": "Need `sdpOffer` to be passed in the data." });
     }
 
+    var pipelineretrieved = function (_pipeline) {
+        console.log("MediaPipeline retrieved", _pipeline.id);
+
+        pipeline = _pipeline;
+        cq.kurento.getMediaobjectById(session.hub.id).then(hubretrieved, onerror);
+    };
+
+    var hubretrieved = function (_hub) {
+        console.log("Hub retrieved", _hub.id);
+
+        hub = _hub;
+        cq.kurento.create("HubPort", { hub: hub }).then(hubportcreated, onerror);
+    };
+
+    var hubportcreated = function (_hubport) {
+        console.log("Hubport created", _hubport.id);
+
+        hubport = _hubport;
+        pipeline.create("WebRtcEndpoint").then(webrtcendpointcreated, onerror);
+    };
+
+    var webrtcendpointcreated = function (webrtc) {
+        console.log("WebRtcEndpoint created", webrtc.id);
+
+        webrtc.processOffer(sdpoffer).then(function (sdpanswer) {
+
+            webrtc.connect(hubport).then(function () {
+                hubport.connect(webrtc).then(function () {
+                    joinsession(webrtc, sdpanswer);
+                }, onerror);
+            }, onerror);
+
+        }, onerror);
+    };
+
+    var joinsession = function (webrtc, sdpanswer) {
+        var datetime = new Date();
+        // Prepare data object
+        session.members.push({ ip: socket.client.conn.remoteAddress, name: name, joined: datetime, quit: null,
+                               hubport: hubport, webrtc: webrtc });
+
+        // Put id in session db
+        cq.db.sessions.put(id, session, function (err) {
+            if (err) {
+                return onerror(err);
+            }
+            console.log("Session joined", id);
+            return callback(null, { "message": "Session joined",
+                                    session: { id: id,
+                                               data: data,
+                                               sdpAnswer: sdpanswer }});
+
+
+        });
+    };
+
     // Fetch session
-    cq.db.sessions.get(id, function (err, session) {
+    cq.db.sessions.get(id, function (err, _session) {
         if (err) {
-            console.log("[ERR] Fetching session", id, err);
+            console.er("[ERR] Fetching session", id, err);
             return callback({ "message": "Not right." });
         }
+
+        session = _session;
 
         // Match name
         var namematch = session.members.filter(function (el) {
@@ -40,86 +106,6 @@ module.exports = function (data, callback, socket) {
 
         console.log("Connecting %s to call id %s. Retrieving pipeline.", name, id);
 
-        cq.kurento.getMediaobjectById(session.pipeline.id, function (error, pipeline) {
-            if (error) {
-                console.log("[ERR] Fetching pipeline for session", id, err);
-                return callback({ "message": "Can't join session." });
-            }
-
-            console.log("Retrieved pipeline", pipeline.id);
-            console.log("Retrieving hub", session.hub.id);
-
-            cq.kurento.getMediaobjectById(session.hub.id, function (error, hub) {
-                if (error) {
-                    console.log("[ERR] Fetching hub for session", id, err);
-                    return callback({ "message": "Can't join session." });
-                }
-
-                console.log("Retrieved hub", hub.id);
-                console.log("Creating new HubPort for hub", hub.id);
-
-                cq.kurento.create("HubPort", { "hub": hub }, function (error, hubport) {
-                    if (error) {
-                        console.log("HubPort error", error);
-                        return callback({ "message": "Can't join session." });
-                    }
-                    console.log("HubPort created", hubport.id);
-
-                    pipeline.create("WebRtcEndpoint", function (error, webrtc) {
-                        if (error) {
-                            console.log("WebRtcEndpoint error", error);
-                            return callback({ "message": "Can't join session'." });
-                        }
-                        console.log("WebRtcEndpoint created", webrtc.id);
-
-                        webrtc.processOffer(sdpoffer, function (err, sdpanswer) {
-                            if (error) {
-                                console.log("WebRtcEndpoint error", error);
-                                return callback({ "message": "Oops! Error! API has gone nuts." });
-                            }
-
-                            webrtc.connect(hubport, function (error) {
-                                if (error) {
-                                    console.log("WebRTC->HubPort connection error", error);
-                                    return callback({ "message": "Oops! Error! API has gone nuts." });
-                                }
-
-                                console.log("WebRTCEndpoint %s connected to Hubport %s", hubport.id, webrtc.id);
-
-                                hubport.connect(webrtc, function (error) {
-                                    if (error) {
-                                        console.log("Hubport->WebRTC connection error", error);
-                                        return callback({ "message": "Oops! Error! API has gone nuts." });
-                                    }
-
-                                    console.log("Hubport %s connected to WebRTCEndpoint %s", hubport.id, webrtc.id);
-
-                                    // Create new user info
-                                    var currentuser = { ip: socket.client.conn.remoteAddress, name: name,
-                                                        joined: Date.now(), quit: null, hubport: hubport, webrtc: webrtc };
-
-                                    session.members.push(currentuser);
-
-                                    // Add to current session info
-                                    cq.db.sessions.put(id, session, function (err) {
-                                        if (err) {
-                                            console.log("[ERR] Updating session", id, err);
-                                            return callback({ "message": "Can't join session." });
-                                        }
-
-                                        return callback(null, { "message": "Session joined",
-                                                            "session": {
-                                                                "id": id,
-                                                                "data": data,
-                                                                "sdpAnswer": sdpanswer
-                                                            }});
-                                    });
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        });
+        cq.kurento.getMediaobjectById(session.pipeline.id).then(pipelineretrieved, onerror);
     });
 };
