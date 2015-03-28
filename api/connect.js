e9ec255f1298422e23999a76972e2465cac9a262
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-03-28 21:10:39 +0530

    Implement connecting to existing calls
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- api/connect.js
+++ api/connect.js
@@ -4,14 +4,10 @@
  * Connect to existing connections
  */
 
+var crypto = require("crypto");
+
 module.exports = function (data, callback, socket) {
-    var id = data && data.sessionid && data.sessionid.trim(),
-        name = data && data.name && data.name.trim(),
-        sdpoffer = data && data.sdpOffer && data.sdpOffer.trim(),
-        pipeline = null,
-        hub = null,
-        hubport = null,
-        session = null;
+    var id = data && data.sessionid && data.sessionid.trim();
 
     var onerror = function (error) {
         console.log("ERROR", error);
@@ -23,68 +19,21 @@ module.exports = function (data, callback, socket) {
         socket.emit("message", { value: value, type: type });
     };
 
+    var createstring = function () {
+        return crypto.createHash("sha1")
+            .update(Date.now().toString() + Math.random().toString())
+            .digest('hex')
+            .slice(0, 8);
+    };
+
     if (!id) {
         return callback({ "message": "Need `sessionid` to be passed in the data." });
     }
 
-    if (!name) {
-        return callback({ "message": "Need `name` to be passed in the data." });
-    }
-
-    if (!sdpoffer) {
-        return callback({ "message": "Need `sdpOffer` to be passed in the data." });
-    }
-
-    var pipelineretrieved = function (_pipeline) {
-        console.log("MediaPipeline retrieved", _pipeline.id);
-        message("Obtained call pipeline");
-
-        pipeline = _pipeline;
-        cq.kurento.getMediaobjectById(session.hub.id).then(hubretrieved, onerror);
-    };
-
-    var hubretrieved = function (_hub) {
-        console.log("Hub retrieved", _hub.id);
-        message("Obtained call hub");
-
-        hub = _hub;
-
-        cq.kurento.create("HubPort", { hub: hub }).then(hubportcreated, onerror);
-    };
-
-    var hubportcreated = function (_hubport) {
-        console.log("Hubport created", _hubport.id);
-        message("Hubport created");
-
-        hubport = _hubport;
-        pipeline.create("WebRtcEndpoint").then(webrtcendpointcreated, onerror);
-    };
-
-    var webrtcendpointcreated = function (webrtc) {
-        console.log("WebRtcEndpoint created", webrtc.id);
-        message("WebRTC Endpoint created");
-
-        webrtc.processOffer(sdpoffer).then(function (sdpanswer) {
-            message("SDP offer accepted");
-
-            webrtc.connect(hubport).then(function () {
-                console.log("WebRTC Endpoint connected to Hubport " + hubport.id);
-
-                hubport.connect(webrtc).then(function () {
-                    console.log("Hubport connected to WebRTC Endpoint.");
-                    message("Connections established");
-                    joinsession(webrtc, sdpanswer);
-                }, onerror);
-
-            }, onerror);
-        }, onerror);
-    };
-
-    var joinsession = function (webrtc, sdpanswer) {
+    var updatesession = function (session, username, token) {
         var datetime = new Date();
-        // Prepare data object
-        session.members.push({ ip: socket.client.conn.remoteAddress, name: name, joined: datetime, quit: null,
-                               hubport: hubport, webrtc: webrtc });
+
+        session.members[username] = { name: username, joined: datetime, quit: null, token: token };
 
         // Put id in session db
         cq.db.sessions.put(id, session, function (err) {
@@ -92,35 +41,44 @@ module.exports = function (data, callback, socket) {
                 return onerror(err);
             }
             console.log("Session joined", id);
-            return callback(null, { "message": "Session joined",
+            return callback(null, { message: "Session joined",
                                     session: { id: id,
-                                               data: data,
-                                               sdpAnswer: sdpanswer }});
-
-
+                                               data: session },
+                                    username: username,
+                                    token: token });
         });
     };
 
     // Fetch session
-    cq.db.sessions.get(id, function (err, _session) {
+    cq.db.sessions.get(id, function (err, session) {
         if (err) {
             console.er("[ERR] Fetching session", id, err);
             return callback({ "message": "Not right." });
         }
 
-        session = _session;
+        var username = createstring();
 
-        // Match name
-        var namematch = session.members.filter(function (el) {
-            return el.name === name;
-        });
-
-        if (namematch.length) {
-            return callback({ "message": "Name already in use. Could not join session." });
+        // Check for uxistingname name
+        if (session.members[username]) {
+            username = createstring();
         }
 
-        console.log("Connecting %s to call id %s. Retrieving pipeline.", name, id);
+        console.log("Connecting %s to call id %s. Retrieving room.", username, id);
 
-        cq.kurento.getMediaobjectById(session.pipeline.id).then(pipelineretrieved, onerror);
+        // Retrieve room
+        cq.nuve.API.getRoom(session.room._id, function (room) {
+            room = JSON.parse(room);
+            message("Found call. Connecting.");
+            console.log("Retrieved room");
+
+            //Create token
+            cq.nuve.API.createToken(room._id, username, "audiocaller", function (token) {
+                // Got user token
+                message("Generated token");
+                console.log("Got user token. User: %s, Call: %s", username, id);
+                updatesession(session, username, token);
+            }, onerror);
+
+        }, onerror);
     });
 };
