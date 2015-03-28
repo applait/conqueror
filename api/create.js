258cb811a84449802de1bb84f3f28c9d2ae050da
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-03-28 21:09:24 +0530

    First take at implementing licode rooms with conqueror
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- api/create.js
+++ api/create.js
@@ -17,100 +17,58 @@ module.exports = function (data, callback, socket) {
         socket.emit("message", { value: value, type: type });
     };
 
-    var id = crypto.createHash("sha1")
+    var createstring = function () {
+        return crypto.createHash("sha1")
             .update(Date.now().toString() + Math.random().toString())
             .digest('hex')
             .slice(0, 8);
-
-    // Look for the `name` query parameter
-    var name = data && data.name && data.name.trim();
-    var sdpoffer = data && data.sdpOffer && data.sdpOffer.trim();
-    var pipeline = null,
-        hub = null,
-        hubport = null;
-
-    if (!name) {
-        return callback({ "message": "Need `name` to be passed in the data." });
-    }
-
-    if (!sdpoffer) {
-        return callback({ "message": "Need `sdpOffer` to be passed in the data." });
-    }
-
-    var pipelinecreated = function (_pipeline) {
-        console.log("MediaPipeline created", _pipeline.id);
-        message("Pipeline created");
-
-        pipeline = _pipeline;
-        pipeline.create("Composite").then(hubcreated, onerror);
     };
 
-    var hubcreated = function (_hub) {
-        console.log("Hub Created", _hub.id);
-        message("Hub created");
+    var id = createstring();
 
-        hub = _hub;
-        cq.kurento.create("HubPort", { hub: hub }).then(hubportcreated, onerror);
-    };
-
-    var hubportcreated = function (_hubport) {
-        console.log("Hubport created", _hubport.id);
-        message("Hubport created");
-
-        hubport = _hubport;
-        pipeline.create("WebRtcEndpoint").then(webrtcendpointcreated, onerror);
-    };
-
-    var webrtcendpointcreated = function (webrtc) {
-        console.log("WebRtcEndpoint created", webrtc.id);
-        message("WebRTC Endpoint created");
-
-        webrtc.processOffer(sdpoffer).then(function (sdpanswer) {
-            console.log("SDP offer accepted");
-            message("SDP offer accepted");
-
-            webrtc.connect(hubport).then(function () {
-                console.log("WebRTC Endpoint connected to Hubport " + hubport.id);
-
-                hubport.connect(webrtc).then(function () {
-                    console.log("Hubport connected to WebRTC Endpoint.");
-                    message("Connections established");
-                    createsession(webrtc, sdpanswer);
-                }, onerror);
-
-            }, onerror);
-
-        }, onerror);
-    };
-
-    var createsession = function (webrtc, sdpanswer) {
+    var createsession = function (room, username, token) {
         var datetime = new Date();
         // Prepare data object
-        var data = {
-            members: [{ ip: socket.client.conn.remoteAddress, name: name, joined: datetime, quit: null,
-                        hubport: hubport, webrtc: webrtc }],
+        var sessiondata = {
+            room: room,
+            members: {},
             meta: {
                 created: datetime,
-                creator: name
-            },
-            pipeline: pipeline,
-            hub: hub
+                creator: username
+            }
         };
 
+        sessiondata.members[username] =  { name: username, joined: datetime, quit: null, token: token };
+
         // Put id in session db
-        cq.db.sessions.put(id, data, function (err) {
+        cq.db.sessions.put(id, sessiondata, function (err) {
             if (err) {
                 return onerror(err);
             }
-            console.log("Session created", id, data.meta.created);
-            return callback(null, { "message": "Session created",
+            console.log("Session created", id, sessiondata.meta.created);
+            return callback(null, { message: "Session created",
                                     session: { id: id,
-                                               data: data,
-                                               sdpAnswer: sdpanswer }});
+                                               data: sessiondata },
+                                    username: username,
+                                    token: token });
 
 
         });
     };
 
-    cq.kurento.create("MediaPipeline").then(pipelinecreated, onerror);
+    // Create call room here
+    console.log("Beginning call: ", id);
+    cq.nuve.API.createRoom(id, function (room) {
+        message("Room created");
+        console.log("Nuve Room created ", room._id);
+        // Room created. Connect current user
+        var username = createstring();
+        cq.nuve.API.createToken(room._id, username, "audiocaller", function (token) {
+            // Got user token
+            message("Generated token");
+            console.log("Got user token. User: %s, Call: %s", username, id);
+            createsession(room, username, token);
+        }, onerror);
+
+    }, onerror);
 };
