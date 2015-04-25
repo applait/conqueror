589d7ca894826a8db4b667986a571e7d396c6fa5
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-25 18:45:48 +0530

    Move Conqueror to HTTPS. Fix #13
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var express = require("express"),
+    https = require("https"),
+    fs = require("fs"),
     path = require("path"),
     bodyParser = require("body-parser"),
     nuve = require("./nuve");
@@ -22,8 +24,14 @@ global.cq = {
 
 // Configure application
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    // Lock down domains
+    if (config.ALLOWED_ORIGINS.indexOf(req.headers["host"]) < 0) {
+        console.log("Blocked connection from: %s. User agent: %s", req.headers["host"], req.headers["user-agent"]);
+        return res.status(403).send("Invalid origin");
+    }
+    console.log("Allowing connection from: %s. User agent: %s", req.headers["host"], req.headers["user-agent"]);
+    res.header('Access-Control-Allow-Origin', req.headers.host);
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     return next();
 });
@@ -31,14 +39,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname, 'static')));
 
-// Start the server
-var server = app.listen(config.APP_PORT, config.APP_IP, function () {
-    console.log("Starting ConQueror...");
-    console.log("Listening on port %d", server.address().port);
+// Set https options
+var https_options = {
+  key: fs.readFileSync(config.SSL_KEY),
+  cert: fs.readFileSync(config.SSL_CERT),
+  passphrase: config.SSL_PASSPHRASE
+};
+
+// Start the secure server
+var server = https.createServer(https_options, app).listen(config.APP_PORT, config.APP_IP, function () {
+  console.log("Starting Conqueror secure server...");
+  console.log("Listening on %s:%d", config.APP_IP, config.APP_PORT);
 });
 
 var io = require("socket.io").listen(server);
 io.set("log level", 1);
+io.set("origins", config.ALLOWED_ORIGINS);
 io.sockets.on("connection", function (socket) {
     api(socket);
 });
